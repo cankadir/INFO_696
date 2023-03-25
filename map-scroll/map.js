985baf5fd6ffb7c06cd2077758f66293eff8790c
Can Sucuoğlu <cansucuoglu@gmail.com>
2023-03-25 15:04:00 -0400

    Added Map Story

---
+++ map-scroll/map.js
@@ -0,0 +1,93 @@
+
+
+// Manually generated data
+let data = [
+    { "country": "Oakland", "what": "I was born here.", "year": 1983 , lat:'37.8044', lon:'-122.2711', photo:"https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.elitetranslingo.com%2Fwp-content%2Fuploads%2F2014%2F10%2FOaklandca.jpg&f=1&nofb=1&ipt=bf19acef8fd433760bcf944d9c2b0bdcfe349a38cf9bc8965b5b6f85e3cf5447&ipo=images"},
+    { "country": "Princeton", "what": "I lived here for 3 years", "year": 1984, lat:'40.3488', lon:'-74.6598'},
+    { "country": "Ankara", "what": "I lived most of my childhood in Ankara", "year": 1986, lat:'39.9334', lon:'32.8597', photo:"https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fjustfunfacts.com%2Fwp-content%2Fuploads%2F2019%2F11%2Fankara-castle.jpg&f=1&nofb=1&ipt=d62cce7ced112749354f2d89b838daa432cee454a5306b5086fd51945ede81d9&ipo=images"},
+    { "country": "Istanbul", "what": "Architecture school from 17 to 22 years old.", "year": 2000, lat:'41.0082', lon:'28.9784', photo:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.ne4YYP5BUNbHqdh7stPiQgAAAA%26pid%3DApi&f=1&ipt=198a4b00206eb0d7c02fad4791e22f1ed77581a2e94aa15405d9dfb3274032e8&ipo=images"},
+    { "country": "Los Angeles", "what": "Did March and then started working. LA was not very nice back then. I don't remember the place very fondly.", "year": 2006, lat:'34.0489', lon:'-118.242'}, 
+    { "country": "Warsaw", "what": "Worked Here", "year": 2009, lat:'52.2300', lon:'21.0254', photo:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flive.staticflickr.com%2F1940%2F30510793907_08b96ca862.jpg&f=1&nofb=1&ipt=92d4f5995e91edaec557d5abe89ecff75cf225cadd5725679461768c81a2e4f7&ipo=images'}, 
+    { "country": "Istanbul", "what": "Back Home", "year": 2011, lat:'41.0082', lon:'28.97842'},
+    { "country": "New York", "what": "Lev was born and I started to worked at Pratt.Now working at Numina and Lev is 3 years old already.", "year": 2019, lat:'40.7128', lon:'-74.0060'},
+]
+
+// ----- Create a map
+//create a leaflet map
+var map = L.map('map').setView([data[0].lat, data[0].lon], 8);
+
+//add a tile layer to the map
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// ----- Create DOM ELEMENTS based on data
+// Add and article to the scrolly div
+const scrolly = d3.select('.scrolly');
+const article = scrolly.append("article")
+
+data.forEach(function(d) {
+    let step = article.append("div")
+        .attr("class","step")
+        .attr("data-index" , `${d.year}`);
+    let stepCont = step.append("div")
+        .attr("class","step-content")
+    stepCont.append("p")
+        .html( `<b>${d.country},${d.year}</b><br><br>${d.what}` )
+    
+    if (d.photo){
+        stepCont.append("img")
+            .attr("src", d.photo)
+            .attr("width", "100%")
+            .attr("class","city-photo")
+    }
+});
+
+// ----- SCROLL ACTION
+const container = d3.select('#scrolly-overlay');
+const stepSel = container.selectAll('.step');
+
+// This is where all scroll related actions happen. 
+function updateChart(el) {
+    const index = d3.select(el).attr('data-index');
+    console.log(index);
+	const sel = container.select(`[data-index='${index}']`);
+	
+    // Get Data by index
+    let dataIndex = sel.attr('data-index');
+    // let value = data[dataIndex];
+    console.log(dataIndex);
+    let value = data.filter(d => d.year == dataIndex)[0];
+    let lat = value.lat;
+    let lon = value.lon; 
+
+    // assign is-active class to the current step
+    // This connectes to CSS
+	stepSel.classed('is-active', (d,i)=>{
+        // Check each .step if it matches the index
+        let stepIndex = stepSel["_groups"][0][i].getAttribute("data-index");
+        if (stepIndex === index){return true}
+        else {return false}
+
+    }); 
+    
+    // Do Something.
+    // Move the map
+    map.flyTo([lat,lon], 8);
+}
+
+function init() {
+
+	enterView({
+		selector: stepSel.nodes(),
+		offset: 0.25, // When does the floating box gets activated
+		enter: el => {
+			updateChart(el);
+		},
+		exit: el => {
+			updateChart(el);
+		}
+	});
+}
+
+init();
